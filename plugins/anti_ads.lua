e87ff7f9f602d7c161f36210721323ee597e95e3
EVILBOT <danyal.jafari2014@gmail.com>
2016-03-04 01:11:09 +0330

    Update anti_ads.lua

--- plugins/anti_ads.lua
+++ plugins/anti_ads.lua
@@ -29,8 +29,3 @@ return {patterns = {
 "[Ww][Ww][Ww]:",
 "عضویت",
 }, run = run}
-
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
